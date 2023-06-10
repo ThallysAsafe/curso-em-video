db6c143294204482c6170f98434a2ac20d09f91e
Thallys Asafe <thallysribeiro37@gmail.com>
2023-06-10 13:38:57 -0300

    Editando alguns exercicios

--- PythonExercicios/ds033.py
+++ PythonExercicios/ds033.py
@@ -1,15 +1,29 @@
-n1 = float(input('Digite o primeiro numero: '))
-n2 = float(input('Digite o segundo numero: '))
-n3 = float(input('Digite o terceiro numero: '))
-if n1 < n2 < n3:
-    print('O menor numero é {} e o maior numero é {}'.format(n1, n3))
-elif n3 < n2 < n1:
-    print('O menor numero é {} e o maior numero é {}'.format(n3, n1))
-elif n2 < n1 < n3:
-    print('O menor numero é {} e o maior numero é {}'.format(n2, n3))
-elif n3 < n1 < n2:
-    print('O menor numero é {} e o maior numero é{}'.format(n3, n2))
-elif n1 < n3 < n2:
-    print('O menor numero é {} e o maior numero é {}'.format(n1, n2))
-else:
-    print('O menor numero é {} e o maior numero é {}'.format(n2, n1))
+a = float(input('Digite o primeiro numero: '))
+b = float(input('Digite o segundo numero: '))
+c = float(input('Digite o terceiro numero: '))
+# Verificando quem é o menor!
+menor = a
+if b < a and b < c:
+    menor = b
+if c < a and c < b:
+    menor = c
+# Verificando quem é o maior!
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
+print('O menor valor digitado foi {}'.format(menor))
+print('O maior valor digitado foi {}'.format(maior))
+# if n1 < n2 < n3:
+#    print('O menor numero é {} e o maior numero é {}'.format(n1, n3))
+# elif n3 < n2 < n1:
+#    print('O menor numero é {} e o maior numero é {}'.format(n3, n1))
+# elif n2 < n1 < n3:
+#    print('O menor numero é {} e o maior numero é {}'.format(n2, n3))
+# elif n3 < n1 < n2:
+#   print('O menor numero é {} e o maior numero é {}'.format(n3, n2))
+# elif n1 < n3 < n2:
+#   print('O menor numero é {} e o maior numero é {}'.format(n1, n2))
+# else:
+#   print('O menor numero é {} e o maior numero é {}'.format(n2, n1))
--- PythonExercicios/ds035.py
+++ PythonExercicios/ds035.py
@@ -1,7 +1,7 @@
 r1 = float(input('Digite o comprimento da primeira reta: '))
 r2 = float(input('Digite o comprimento da segunda reta: '))
 r3 = float(input('Digite o comprimento da terceira reta: '))
-if(r1 + r2) > r3 > (r1 - r2) * -1:
+if(r1 + r2) > r3 and r1 + r3 > r2 and r2 + r3 > r1:
     print('Essas retas são capazes de formar um triangulo')
 else:
     print('Essas retas não são capazes de formar um triangulo')
